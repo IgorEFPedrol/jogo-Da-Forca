ca8f5879bf972f77096ba7e4f1a98a71d794751a
Igor Enrico <igorefp@gmail.com>
2023-06-06 17:17:30 -0300

    Finalizei o projeto

--- jogoDaForca.py
+++ jogoDaForca.py
@@ -1,17 +1,31 @@
 import random
 
 palavras = ["primeiro", "carlos", "jogar", "pizza", "hamburguer", "riacho", "sol", "andar", "correr", "ultimo"]
+vidas = 6
+display = []
 
 randomPalavra = random.choice(palavras)
 
-print(randomPalavra)
+for n in range(len(randomPalavra)):
+    display.append("_")
+print(display)
 
-num = int(input('Insira um número: '))
+while vidas > 0:
+    chute = input("Digite uma letra: ")
+    
+    cont = 0
+    acertou = False
+    for letra in randomPalavra:
+        if chute == letra:
+            display[cont] = chute
+            acertou = True    
+        cont += 1
+    if not acertou:
+        vidas -= 1
+    print(display)        
+    print(vidas)
+    if '_' not in display:
+        print("Você ganhou!!")
+        break
 
-if num %2 == 0:
-    print(f"{num} é par")
-else:
-    print(f"{num} é ímpar") 
-
-for num in range(len(palavras)):
-    print(palavras[num])
+print("Você perdeu!!!\nA palavra era " + randomPalavra)
